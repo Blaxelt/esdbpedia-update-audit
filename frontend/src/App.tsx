9d978885b81dd90bb047674893e5636e44eb0d33
Blaxelt <docbriancw@gmail.com>
2026-02-28 13:15:55 +0100

    feat: implemented iframe for wikipedia articles and search function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [articleId, setArticleId] = useState('171311417')
+  const [inputValue, setInputValue] = useState('')
+  const [error, setError] = useState('')
+
+  const handleSearch = () => {
+    if (!inputValue.trim()) {
+      setError('Enter an article ID')
+      return
+    }
+    if (isNaN(Number(inputValue))) {
+      setError('ID must be a number')
+      return
+    }
+    setError('')
+    setArticleId(inputValue)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="top-bar">
+        <p>Article ID: {articleId}</p>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder='Article ID...'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button onClick={handleSearch}>🔎</button>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="panels">
+        <div className="clean-panel">
+        </div>
+        <div className="original-panel">
+          <iframe
+            src={`https://es.wikipedia.org/w/index.php?oldid=${articleId}`}
+            className="original-iframe"
+            title="Wikipedia Article"
+          />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
